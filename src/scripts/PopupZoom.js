e74357061d65bb13bf2a16fa0099a81519b848b0
Biryukov Sergey <2700036@mail.ru>
2020-04-15 20:43:33 +0300

    Looks good

--- src/scripts/PopupZoom.js
+++ src/scripts/PopupZoom.js
@@ -1,4 +1,5 @@
-class PopupZoom extends Popup {
+import {Popup} from './Popup.js';
+export class PopupZoom extends Popup {
   open(event) {
     if (event.target.classList.contains("place-card__image") || event.target.classList.contains("user-info__photo")) {
       document.querySelector(".popup-zoom").classList.add("popup_is-opened");
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,13 @@
 import '../pages/index.css';
-
-(function() {
+import {Api} from './API.js';
+import {Card} from './Card.js';
+import {CardList} from './CardList.js';
+import {PopupNewPlace} from './PopupNewPlace.js';
+import {PopupEdit} from './PopupEdit.js';
+import {PopupZoom} from './PopupZoom.js';
+import {UserInfo} from './UserInfo.js';
+import {FormValidator} from './FormValidator.js';
+ 
   const placesList = document.querySelector(".places-list");
   const root = document.querySelector(".root");
 
@@ -24,13 +31,7 @@ import '../pages/index.css';
   const card = new Card(api);
   const cardList = new CardList(placesList, api, card, userInfo);
 
-  const popupEditObj = new PopupEdit(
-    editInfoButton,
-    popupEdit,
-    api,
-    //REVIEW2. Надо передавать в качестве аргумента просто экземпляр userInfo и в PopupEdit вызывать с ним и setUserInfo и updateUserInfo. --- OK
-    userInfo
-  );
+  const popupEditObj = new PopupEdit(editInfoButton, popupEdit, api, userInfo);
   const popupAddObj = new PopupNewPlace(
     addFormButton,
     popupPlaceAdd,
@@ -42,6 +43,15 @@ import '../pages/index.css';
 
   const editFormvalidator = new FormValidator(editForm);
   const NewPlaceFormvalidator = new FormValidator(newPlaceForm);
+
+  function close() {
+    if (
+      event.target.classList.contains("popup") ||
+      event.target.classList.contains("popup__close")
+    ) {
+      event.target.closest(".popup").classList.remove("popup_is-opened");
+    }
+  }
 
   function usersLikeRender(event, res) {
     const card = event.target.closest(".place-card");
@@ -59,13 +69,48 @@ import '../pages/index.css';
     Card.showLikes(card);
   }
 
-  function close() {
-    if (
-      event.target.classList.contains("popup") ||
-      event.target.classList.contains("popup__close")
-    ) {
-      event.target.closest(".popup").classList.remove("popup_is-opened");
-    }
+  function setLikeDeleteListener(placesList, api, usersLikeRender, card) {
+    placesList.addEventListener("click", event => {
+      if (event.target.classList.contains("place-card__like-icon")) {
+        if (!event.target.classList.contains("place-card__like-icon_liked")) {
+          api
+            .putLike(
+              event.target.closest(".place-card").getAttribute("data-id")
+            )
+            .then(res => {
+              usersLikeRender(event, res);
+            })
+            .catch(err => {
+              console.log(err);
+            });
+          card.like(event);
+        } else {
+          api
+            .deleteLike(
+              event.target.closest(".place-card").getAttribute("data-id")
+            )
+            .then(res => {
+              usersLikeRender(event, res);
+            })
+            .catch(err => {
+              console.log(err);
+            });
+          card.like(event);
+        }
+      }
+      if (event.target.classList.contains("place-card__delete-icon")) {
+        api
+          .deleteCard(
+            event.target.closest(".place-card").getAttribute("data-id")
+          )
+          .then(_res => {
+            card.remove(event);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
+    });
   }
 
   api
@@ -84,71 +129,19 @@ import '../pages/index.css';
       console.log(err);
     });
 
-  function usersLikeRender(event, res) {
-    const card = event.target.closest(".place-card");
-    Card.likesNumbers(card, res);
-    const usersLikes = [];
-    function notLiked(likes) {
-      if (likes.length === 0) {
-        return "Это место пока никому не понравилось...";
-      } else {
-        return `Нравится: ${likes.join(", ")}`;
-      }
-    }
-    res.likes.forEach(e => usersLikes.push(e.name));
-    card.setAttribute("data-likes", `${notLiked(usersLikes)}`);
-    Card.showLikes(card);
-  }
-
   setLikeDeleteListener(placesList, api, usersLikeRender, card);
 
   placesList.addEventListener("mouseover", function(event) {
-    if (event.target.classList.contains("place-card__like-icon")) {
+    if (
+      event.target.classList.contains("place-card__like-icon") ||
+      event.target.classList.contains("like-bubble-shown")
+    ) {
       event.target.nextElementSibling.classList.add("like-bubble-shown");
     } else {
       const hideAll = Array.from(document.querySelectorAll(".like-bubble"));
       hideAll.forEach(el => el.classList.remove("like-bubble-shown"));
     }
   });
-
-  function setLikeDeleteListener(placesList, api, usersLikeRender, card) {
-    placesList.addEventListener("click", event => {
-      if (event.target.classList.contains("place-card__like-icon")) {
-        if (!event.target.classList.contains("place-card__like-icon_liked")) {
-          api
-            .putLike(event.target.closest(".place-card").getAttribute("data-id"))
-            .then(res => {
-              usersLikeRender(event, res);
-            })
-            .catch(err => {
-              console.log(err);
-            });
-          card.like(event);
-        }
-        else {
-          api
-            .deleteLike(event.target.closest(".place-card").getAttribute("data-id"))
-            .then(res => {
-              usersLikeRender(event, res);
-            })
-            .catch(err => {
-              console.log(err);
-            });
-          card.like(event);
-        }
-      }
-      if (event.target.classList.contains("place-card__delete-icon")) {
-        api
-          .deleteCard(event.target.closest(".place-card").getAttribute("data-id"))
-          .then(_res => {
-            card.remove(event);
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      }
-    });
-  }
 
   window.addEventListener("keydown", function(event) {
     if (event.key === "Escape") {
@@ -158,6 +151,4 @@ import '../pages/index.css';
     }
   });
   window.addEventListener("click", close);
-})();
-
 
